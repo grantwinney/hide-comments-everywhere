63cfd397e0a2e48508670838bfdb55020d90d026
Grant Winney <grantwinney@gmail.com>
2017-10-18 19:52:16 -0400

    definitions aren't automatically downloading on a fresh install

--- js/background.js
+++ js/background.js
@@ -23,24 +23,17 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 chrome.windows.onCreated.addListener(function() {
-    // if filters are missing, download them
-    // if filters are present but version is missing or old, re-download them
-    chrome.storage.local.get('site_patterns', function(result) {
-        if (result == undefined || result.site_patterns == undefined) {
-            getAndStoreSiteDefinitions();
-        } else {
-            getDefinitionVersion(function(version) {
-                chrome.storage.local.get('definition_version', function(result) {
-                    if (result == undefined || result.definition_version == undefined || result.definition_version < version) {
-                        getAndStoreSiteDefinitions(version);
-                    }
-                });
-            });
-        }
-    });
-    // set the browser action to either show a popup or not, based on user's setting
-    chrome.storage.local.get('one_click_option', function(result) {
-        var oneClickEnabled = (result != undefined && result.one_click_option == true);
-        chrome.browserAction.setPopup({popup: oneClickEnabled ? "" : "../popup.html"});
+    getDefinitionVersion(function(version) {
+        chrome.storage.local.get('definition_version', function(result) {
+            if (result === undefined || result.definition_version === undefined || result.definition_version < version) {
+                getAndStoreSiteDefinitions(version);
+            }
+        });
     });
 })
+
+chrome.storage.local.get('definition_version', function(result) {
+    if (result === undefined || result.definition_version === undefined) {
+        getAndStoreSiteDefinitions(version);
+    }
+});
