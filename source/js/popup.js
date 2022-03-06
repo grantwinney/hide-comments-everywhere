3345f3f6ec85102319c6d779036636af1700b462
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2022-03-05 23:01:34 -0500

    fill dropdown with available url formats to choose from

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,4 +1,11 @@
-function addToExclusion(tabId) {
+function displayMessage(message) {
+    let alert = document.getElementById('alert');
+    alert.innerHTML = message;
+    alert.style.setProperty('display', 'block');
+    window.setTimeout(function () { alert.style.setProperty('display', 'none'); }, 5000);
+}
+
+function addToWhitelist(tabId) {
     let selection = document.querySelector('input[name="url"]:checked');
     if (selection !== null) {
         let urlToInclude = selection.value;
@@ -34,14 +41,11 @@ function addToExclusion(tabId) {
     }
 }
 
-function displayMessage(message) {
-    let alert = document.getElementById('alert');
-    alert.innerHTML = message;
-    alert.style.setProperty('display', 'block');
-    window.setTimeout(function () { alert.style.setProperty('display', 'none'); }, 5000);
+function addToBlacklist(tabId) {
+    // TODO: Make sure all 3 of these functions still work
 }
 
-function submitUrlForInclusion() {
+function requestAddToBlacklist() {
     let selection = document.querySelector('input[name="url"]:checked');
     if (selection !== null) {
         let urlToInclude = selection.value;
@@ -60,51 +64,63 @@ function submitUrlForInclusion() {
 }
 
 function displayUrlOptions(url) {
-    // let baseUrl = document.getElementById('baseUrl');
-    // let baseUrlDesc = document.getElementById('baseUrlDesc');
-    // baseUrl.value = url.origin;
-    // baseUrlDesc.value = baseUrlDesc.title = url.origin;
-    // baseUrlDesc.addEventListener('click', function() {baseUrl.checked = true;});
-
-    // let fullUrl = document.getElementById('fullUrl');
-    // let fullUrlDesc = document.getElementById('fullUrlDesc');
-    // fullUrl.value = fullUrlDesc.value = fullUrlDesc.title = url.origin + url.pathname;
-    // fullUrlDesc.addEventListener('click', function() {fullUrl.checked = true;});
+    let currentUrlFormatChoices = document.getElementById('currentUrlFormatChoices');
+    let hostnameParts = url.hostname.split('.');
+    if (hostnameParts[0] === 'www') {
+        let newHostname = `${url.protocol}//${url.hostname}`;
+        currentUrlFormatChoices.add(new Option(newHostname, newHostname));
+    } else {
+        for (let i = hostnameParts.length - 2; i >= 0; i--) {
+            let newHostname = `${url.protocol}//${hostnameParts.slice(i, hostnameParts.length).join('.')}`;
+            currentUrlFormatChoices.add(new Option(newHostname, newHostname));
+        }
+    }
+    currentUrlFormatChoices.add(new Option(url.origin + url.pathname, url.origin + url.pathname))
+   
+    let currentUrl = document.getElementById('currentUrl');
+    for (let eventName of ['click', 'focus', 'keydown']) {
+        currentUrlFormatChoices.addEventListener(eventName, function () { currentUrl.checked = true; });
+    }
 
     let customUrl = document.getElementById('customUrl');
     let customUrlDesc = document.getElementById('customUrlDesc');
-    customUrl.value = 'custom';
-    customUrlDesc.addEventListener('click', function () { customUrl.checked = true; });
     customUrlDesc.value = url.origin + url.pathname;
-    // document.getElementById('customUrlHeading').addEventListener('click', function() {customUrl.checked = true;});
-    customUrlDesc.addEventListener('keydown', function (event) {
-        if (event.key !== 'Tab') {
-            customUrl.checked = true;
-        }
-    });
+    for (let eventName of ['click', 'focus', 'keydown']) {
+        customUrlDesc.addEventListener(eventName, function () { customUrl.checked = true; });
+    }
 }
 
 window.addEventListener('DOMContentLoaded', function load(_event) {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        displayUrlOptions(new URL(tabs[0].url));
         let tabId = tabs[0].id;
-        document.getElementById('toggle_hide').addEventListener('click', function () { toggleComments(tabId); });
-        document.getElementById('options').addEventListener('click', function () { chrome.runtime.openOptionsPage(); });
 
+        // Buttons in caption bar
+        document.getElementById('toggle_hide').addEventListener('click', function () {
+            toggleComments(tabId);
+        });
+        document.getElementById('options').addEventListener('click', function () {
+            chrome.runtime.openOptionsPage();
+        });
         document.getElementById('share').addEventListener('click', function () {
             let shareBox = document.getElementById('share_box');
             shareBox.style.visibility = shareBox.style.visibility === 'hidden' ? 'visible' : 'hidden';
         });
+        document.getElementById('report').addEventListener('click', function () {
+            let reportLink = 'https://github.com/grantwinney/hide-comments-everywhere/issues/new?title=I%27d%20like%20to%20report%20a%20problem%20or%20request%20help&body=I%27m%20experiencing%20the%20following%20issue:%20(include%20as%20many%20details%20as%20possible)';
+            window.open(reportLink, '_blank');
+        });
+
+        // Buttons for sharing addon
         document.getElementById('share_twitter').addEventListener('click', function () {
             let twitterLink = `http://twitter.com/share?text=${document.getElementById('share_box_comment').value}`;
             window.open(twitterLink, '_blank');
         });
         document.getElementById('share_facebook').addEventListener('click', function () {
-            let facebookLink = `https://www.facebook.com/sharer/sharer.php?u=https://grantwinney.com/hide-comments-everywhere/`;
+            let facebookLink = 'https://www.facebook.com/sharer/sharer.php?u=https://grantwinney.com/hide-comments-everywhere/';
             window.open(facebookLink, '_blank');
         });
         document.getElementById('share_reddit').addEventListener('click', function () {
-            let redditLink = `http://www.reddit.com/submit?url=https://grantwinney.com/hide-comments-everywhere&title=Lower%20your%20blood%20pressure.%20Hide%20comments%20everywhere.`;
+            let redditLink = 'http://www.reddit.com/submit?url=https://grantwinney.com/hide-comments-everywhere&title=Lower%20your%20blood%20pressure.%20Hide%20comments%20everywhere.';
             window.open(redditLink, '_blank');
         });
         document.getElementById('share_email').addEventListener('click', function () {
@@ -115,7 +131,11 @@ window.addEventListener('DOMContentLoaded', function load(_event) {
             navigator.clipboard.writeText(document.getElementById('share_box_comment').value);
         });
 
-        document.getElementById('submit_for_inclusion').addEventListener('click', submitUrlForInclusion);
-        document.getElementById('add_to_exclusion').addEventListener('click', function () { addToExclusion(tabId); });
+        // Display available url formats to select from
+        displayUrlOptions(new URL(tabs[0].url));
+
+        document.getElementById('add_to_whitelist').addEventListener('click', function () { addToWhitelist(tabId); });
+        document.getElementById('add_to_blacklist').addEventListener('click', function () { addToBlacklist(tabId); });
+        document.getElementById('request_add_to_blacklist').addEventListener('click', requestAddToBlacklist);
     });
 });
